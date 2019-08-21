d7a19db60588970aea157630e3a2343651178ef7
Isaac <grisman13@gmailcom.com>
2019-08-21 20:20:12 +0300

    Film Studio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import scenes from './imgs/';
+
+class App extends React.Component{
+
+  state ={
+    scene: 0,
+    frame: 0,
+    isWaiting: false,
+  }
+
+  nextImage = ()=> {
+    if(this.state.isWaiting) return;
+
+    this.setState({ isWaiting: true});
+
+    setTimeout(()=>{
+
+
+    this.setState({
+      isWaiting: false,
+      frame: (this.state.frame + 1) % 3,
+      scene: this.state.frame === 2 ?
+             (this.state.scene + 1)  % 3 :
+              this.state.scene,
+    })
+  }, 1000);
+}
+
+startLoop = ()=> {
+this.loopInterval =  setInterval(this.nextImage, 1500);
+}
+
+stopLoop = ()=> {
+  clearInterval(this.loopInterval);
+}
+
+  render() {
+    console.log(this.state);
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img onClick={this.nextImage} alt='' className='hero-img'
+            src={scenes[this.state.scene][this.state.frame]} />
+
+          <div className='film-strip'>
+           <div className='edge' />
+            <div className='cell-container' >
+              <img alt='' src={scenes[this.state.scene][0]} style={{
+                  border: '1px solid '+(this.state.frame === 0 ? 'orange' : 'black')
+                }}/>
+
+              <img alt='' src={scenes[this.state.scene][1]} style={{
+                  border: '1px solid '+(this.state.frame === 1 ? 'orange' : 'black')
+                }}/>
+
+              <img alt='' src={scenes[this.state.scene][2]} style={{
+                  border: '1px solid '+(this.state.frame === 2 ? 'orange' : 'black')
+                }}/>
+            </div>
+            <div className='edge' />
+          </div>
+
+
+
+           <button onClick={this.startLoop}>START</button>
+           <button onClick={this.stopLoop}>STOP</button>
+
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/imgs/index.js
@@ -0,0 +1,17 @@
+import scene11 from './scene-1-1.png';
+import scene12 from './scene-1-2.png';
+import scene13 from './scene-1-3.png';
+
+import scene21 from './scene-2-1.png';
+import scene22 from './scene-2-2.png';
+import scene23 from './scene-2-3.png';
+
+import scene31 from './scene-3-1.png';
+import scene32 from './scene-3-2.png';
+import scene33 from './scene-3-3.png';
+
+export default [
+  [scene11, scene12, scene13],
+  [scene21, scene22, scene23],
+  [scene31, scene32, scene33],
+];
